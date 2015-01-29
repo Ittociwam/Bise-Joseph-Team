cd99273adaa9b4cf1a6c62a457c9ea8f10a9f7c2
Robbie Bise <ittociwam@hotmail.com>
2015-01-29 09:39:46 -0700

    added coordinates class and finished location class, wrote testing program in main.

---
+++ src/Model/Coordinates.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+import java.io.Serializable;
+
+/**
+ *
+ * @author Robbie
+ */
+public class Coordinates implements Serializable{
+    private int x;
+    private int y;
+
+    public Coordinates() {
+
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Coordinates other = (Coordinates) obj;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Coordinates{" + "x=" + x + ", y=" + y + '}';
+    }
+    
+    
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+    
+    
+}
