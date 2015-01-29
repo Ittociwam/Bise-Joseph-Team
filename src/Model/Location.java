20ab8e9ec9f39000935f3677770737193eb53338
Robbie Bise <ittociwam@hotmail.com>
2015-01-29 09:39:57 -0700

    added coordinates class and finished location class, wrote testing program in main.

--- src/Model/Location.java
+++ src/Model/Location.java
@@ -6,11 +6,61 @@
 package Model;
 
 import java.io.Serializable;
-import java.util.Objects;
+
 /**
  *
  * @author Robbie
  */
 public class Location implements Serializable{
+        private Coordinates mapCoords;
+    private Coordinates roomCoords;
+
+    public Location() {
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Location other = (Location) obj;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Location{" + "mapCoords=" + mapCoords + ", roomCoords=" + roomCoords + '}';
+    }
+
+    
+    
+    
+    public Coordinates getMapCoords() {
+        return mapCoords;
+    }
+
+    public void setMapCoords(Coordinates mapCoords) {
+        this.mapCoords = mapCoords;
+    }
+
+    public Coordinates getRoomCoords() {
+        return roomCoords;
+    }
+
+    public void setRoomCoords(Coordinates roomCoords) {
+        this.roomCoords = roomCoords;
+    }
+    
+    
+    
     
 }
