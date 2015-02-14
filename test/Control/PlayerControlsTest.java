87befcc70898bc839b52c1e7502d5ab33aee1833
Robbie Bise <ittociwam@hotmail.com>
2015-02-14 15:55:26 -0700

    fixed the attack tests so they worked in the test packages

---
+++ test/Control/PlayerControlsTest.java
@@ -0,0 +1,174 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Control;
+
+import Model.Character;
+import Model.Player;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Robbie
+ */
+public class PlayerControlsTest {
+
+    public PlayerControlsTest() {
+    }
+
+    /**
+     * Test of isAlpha method, of class PlayerControls.
+     */
+    @Test
+    public void testIsAlpha() 
+    {
+        System.out.println("isAlpha");
+        String code = "Hello";
+        PlayerControls instance = new PlayerControls();
+        boolean expResult = true;
+        boolean result = instance.isAlpha(code);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of dicipherCode method, of class PlayerControls.
+     */
+    @Test
+    public void testDicipherCode() 
+    {
+        System.out.println("dicipherCode");
+        String code = "prfne pvcure";
+        PlayerControls instance = new PlayerControls();
+        String expResult = "CESAR CIPHER";
+        String result = instance.dicipherCode(code);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of attack method, of class PlayerControls.
+     */
+    @Test
+    public void testAttack() 
+    {
+
+        // continue fight
+        Character someCharacter = new Character();
+        someCharacter.setAttack(5);
+        someCharacter.setType('e');
+        someCharacter.setHealth(10);
+
+        Player somePlayer = new Player();
+        somePlayer.setAttack(9);
+        somePlayer.setType('p');
+        somePlayer.setHealth(22);
+
+        System.out.println("attack");
+        PlayerControls instance = new PlayerControls();
+
+        int expResult = 0;
+        int result = instance.attack(somePlayer, someCharacter);
+        assertEquals(expResult, result);
+
+        //player is dead
+        someCharacter.setAttack(5);
+        someCharacter.setType('e');
+        someCharacter.setHealth(10);
+
+        somePlayer.setAttack(9);
+        somePlayer.setType('p');
+        somePlayer.setHealth(0);
+
+        System.out.println("attack");
+
+        expResult = 2;
+        result = instance.attack(somePlayer, someCharacter);
+        assertEquals(expResult, result);
+
+        //enemy is dead
+        someCharacter.setAttack(5);
+        someCharacter.setType('e');
+        someCharacter.setHealth(0);
+
+        somePlayer.setAttack(9);
+        somePlayer.setType('p');
+        somePlayer.setHealth(22);
+
+        System.out.println("attack");
+
+        expResult = 1;
+        result = instance.attack(somePlayer, someCharacter);
+        assertEquals(expResult, result);
+
+        //invalid enemy type
+        someCharacter.setAttack(5);
+        someCharacter.setType('w');
+        someCharacter.setHealth(10);
+
+        somePlayer.setAttack(9);
+        somePlayer.setType('p');
+        somePlayer.setHealth(22);
+
+        System.out.println("attack");
+
+        expResult = -1;
+        result = instance.attack(somePlayer, someCharacter);
+        assertEquals(expResult, result);
+
+        //invalid player type
+        someCharacter.setAttack(5);
+        someCharacter.setType('e');
+        someCharacter.setHealth(10);
+
+        somePlayer.setAttack(9);
+        somePlayer.setType('d');
+        somePlayer.setHealth(22);
+
+        System.out.println("attack");
+
+        expResult = -2;
+        result = instance.attack(somePlayer, someCharacter);
+        assertEquals(expResult, result);
+
+        //invalid enemy attack
+        someCharacter.setAttack(-4);
+        someCharacter.setType('e');
+        someCharacter.setHealth(10);
+
+        somePlayer.setAttack(9);
+        somePlayer.setType('p');
+        somePlayer.setHealth(22);
+
+        System.out.println("attack");
+
+        expResult = -3;
+        result = instance.attack(somePlayer, someCharacter);
+        assertEquals(expResult, result);
+
+        //invalid player attack
+        someCharacter.setAttack(7);
+        someCharacter.setType('e');
+        someCharacter.setHealth(10);
+
+        somePlayer.setAttack(-9);
+        somePlayer.setType('p');
+        somePlayer.setHealth(22);
+
+        System.out.println("attack");
+
+        expResult = -4;
+        result = instance.attack(somePlayer, someCharacter);
+        assertEquals(expResult, result);
+
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+
+    }
+
+}
