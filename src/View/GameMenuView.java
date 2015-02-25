9568dcb2abdbe60030b92e0f74db544130772ccf
Robbie Bise <ittociwam@hotmail.com>
2015-02-25 09:56:48 -0700

    updated mainmenu view and move view

--- src/View/GameMenuView.java
+++ src/View/GameMenuView.java
@@ -4,6 +4,8 @@
  * and open the template in the editor.
  */
 package View;
+import View.ItemView;
+import java.util.Scanner;
 
 /**
  *
@@ -11,8 +13,70 @@ package View;
  */
 public class GameMenuView {
 
-    void displayMenu() {
-        System.out.println("***displayMenu function called ***\n");
+        itemView itemView = new itemView();
+        private final String GAMEMENU = "\n"
+            + "------------------------------------------\n"
+            + "            Game Menu                     \n"
+            + "------------------------------------------\n"
+            + "          H - Get Help/Display Menu       \n"
+            + "M -  Move Menu (N-north, E-east, S-south, W-west)\n"
+            + "         L - Look around the room         \n"
+            + "             V - View Room                \n"
+            + "            I - View Inventory            \n"
+            + "   Each Item will have a unique letter    \n"
+            + "             E- Exit Menu                 \n"   
+            + "------------------------------------------\n";
+                    
+    void display() {
+    char value = ' ';
+        do {
+            System.out.println(GAMEMENU);
+
+            Scanner keyboard = new Scanner(System.in);
+            value = keyboard.next().charAt(0);
+            value = Character.toUpperCase(value);
+
+            switch (value) {
+                    
+                case 'H':
+                    System.out.println(GAMEMENU);
+                    break;
+                case 'M':
+                    this.move();
+                    break;
+                case 'L':
+                    this.viewRoom();
+                    break;
+                case 'V':
+                    this.viewMap();
+                    break;
+                case 'I':
+                    itemView.openItemMenu();
+                    break;
+                case 'E':
+                    break;
+                default:
+                    System.out.println("Invalid input, Try again\n");
+                            
+            }
+        } while (value != 'E');
+}
+
+    private void move() {
+        
+    }
+
+    private void viewRoom() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private void viewMap() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private void viewInventory() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     }
     
-}
+
