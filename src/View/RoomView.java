6905120d726c029834f3e711b145a5706e374fa0
Robbie <Robbie@10.18.149.37>
2015-03-30 10:03:39 -0600

    began work on displaying room

--- src/View/RoomView.java
+++ src/View/RoomView.java
@@ -11,29 +11,54 @@ import Model.Room;
  *
  * @author Robbie
  */
-public class RoomView extends View{
-    
-    
-    
-    public void display(Room room)
-    {
+public class RoomView extends View {
+
+    public void display(Room room) {
         int col = room.getSize().height;
         int row = room.getSize().width;
-        
-        for(int i = 0; i < row; i++)
-            for(int j = 0; i < col; i++)
-            {
-                this.console.println("+-----+-----+-----+-----+-----+\n");
-                this.console.println("|     |     |  ^  |     |     |\n");
-                this.console.println("|     |     |     |     |     |\n");
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; i < col; i++) {
+                if (col == 0) 
+                {
+                    this.console.println("+-----+-----+-----+-----+-----+\n");
+                }
+                if (row == 0) 
+                {
+                    this.console.println("|     |     |  ^  |     |     |\n");
+                } 
+                else 
+                {
+                    this.console.println("|     |     |     |     |     |\n");
+                }
+                if (col == 4) 
+                {
+                    if(row == 2)
+                        this.console.println("|  " + room.elements[col][row].getType() + " >|\n");
+                    else
+                        this.console.println("|  " + room.elements[col][row].getType() + "  |\n");
+                    this.console.println("|     |     |     |     |     |\n");
+                    this.console.println("+-----+-----+-----+-----+-----+\n");
+                } 
+                else 
+                {
+                    if (row == 2 && col == 0) 
+                    {
+                        this.console.println("|< " + room.elements[col][row].getType() + "  ");
+                    } else 
+                    {
+                        this.console.println("|  " + room.elements[col][row].getType() + "  ");
+                    }
+                }
+
             }
+        }
     }
 
-    @Override
-    public void doAction(Object obj) {
+
+@Override
+        public void doAction(Object obj) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-    
-    
-    
+
 }
